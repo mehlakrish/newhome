31251df5f30c5c291bfc6c4e465ca8b73f575a17
krish_mehla <mehlakrish@gmail.com>
2018-10-28 22:51:33 +0530

    form modification

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,39 +12,50 @@ export class HomeComponent implements OnInit {
   user: any;
   userForm: FormGroup;
   submitted = false;
-
+  countries:any = [];
+/*
   countries = [
     {id: 1, name: 'United States'},
     {id: 2, name: "Australia"},
     {id: 3, name: "Canada"},
     {id: 4, name: "Brazil"},
     {id: 5, name: "England"},
-  ];
+  ];*/
  selectedValue = null ;
    constructor(
    private http: Http
-  ) { }
+  ) { 
+
+    this.http.get('http://localhost/test.php').subscribe(res=>{
+    this.countries = res; 
+    console.log(res);
+
+    });
+  }
   
    
   ngOnInit() {
     this.userForm = new FormGroup({
-        userName: new FormControl(''),
-        password: new FormControl(''),
+        userName: new FormControl('',Validators.required),
+        password: new FormControl('',Validators.required),
         country: new FormControl(4),
         sex: new FormControl('m'),
         tmc: new FormControl(true),
     });
   }
-  
-  onSubmit() {
- //   console.log(this.userForm.value);
-   // this.submitted = true;
 
-    this.http.post('http://localhost/test.php',  this.userForm.value).subscribe(res => {
+  onSubmit() {
+ console.log(this.userForm);
+   // this.submitted = true;
+    if(this.userForm.valid){
+      
+      this.http.post('http://localhost/test.php',  this.userForm.value).subscribe(res => {
         console.log(res);
     }, err => {
       console.log(err);
     });
+    }
+    
   }
 
 }
